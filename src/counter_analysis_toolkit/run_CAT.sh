3788e6869907c4098c5ec15e915f4abc9ee68d3d
Daniel Barry <dbarry@vols.utk.edu>
2024-07-31 22:58:55 +0000

    single-threaded run

--- src/counter_analysis_toolkit/run_CAT.sh
+++ src/counter_analysis_toolkit/run_CAT.sh
@@ -5,11 +5,11 @@ PAPIDIR=/home/dbarry/apps/papi-serotonin-zen5
 export LD_LIBRARY_PATH=${PAPIDIR}/lib:${LD_LIBRARY_PATH}
 
 ## Configure OpenMP for the multi-threaded data cache benchmarks.
-export OMP_NUM_THREADS=16
+export OMP_NUM_THREADS=1
 export OMP_PROC_BIND=spread
 
 ## Run the CAT benchmarks.
-for iter in {1..3}
+for iter in {1..1}
 do
     DIR=./output/${TYPE}/${iter}/
     mkdir -p ${DIR}
