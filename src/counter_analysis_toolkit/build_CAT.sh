aff13d47610a1782910c77131d1ee71bfab0b167
Daniel Barry <dbarry@vols.utk.edu>
2025-11-05 11:01:55 -0800

    updates to avoid memory issue

--- src/counter_analysis_toolkit/build_CAT.sh
+++ src/counter_analysis_toolkit/build_CAT.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Set to your PAPI installation.
-PAPIDIR=${PAPIDIR}
+PAPIDIR=${PAPI_DIR}
 export LD_LIBRARY_PATH=${PAPIDIR}/lib:${LD_LIBRARY_PATH}
 
 make realclean
--- src/counter_analysis_toolkit/run_CAT.sh
+++ src/counter_analysis_toolkit/run_CAT.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ## Set to your PAPI installation.
-PAPIDIR=${PAPIDIR}
+PAPIDIR=${PAPI_DIR}
 export LD_LIBRARY_PATH=${PAPIDIR}/lib:${PWD}:${LD_LIBRARY_PATH}
 
 ## Use config file for EMR-specific architectural features.
@@ -16,22 +16,9 @@ export OMP_PLACES="128"
 for iter in {1..1}
 do
     DIR=./output/${TYPE}/${iter}/
-    rm -rf ${DIR}
     mkdir -p ${DIR}
 
     ./cat_collect -in ./input/dcache_presets_1.txt -out ${DIR} -dcr -dcw -instr
-    ./cat_collect -in ./input/branch_presets.txt -out ${DIR} -branch -instr -verbose
-    ./cat_collect -in ./input/flops_presets.txt  -out ${DIR} -flops -vec -instr -verbose
     ./cat_collect -in ./input/dcache_presets_2.txt -out ${DIR} -dcr -dcw -instr
-    ./cat_collect -in ./input/icache_presets.txt -out ${DIR} -ic -instr -verbose
     ./cat_collect -in ./input/dcache_presets_3.txt -out ${DIR} -dcr -dcw -instr
-    ./cat_collect -in ./input/instr_presets.txt  -out ${DIR} -instr -verbose
-
-    ${PAPIDIR}/bin/papi_command_line "PAPI_L1_TCM"  > ${DIR}/tcache_command_line.txt
-    ${PAPIDIR}/bin/papi_command_line "PAPI_L2_TCA" >> ${DIR}/tcache_command_line.txt
-    ${PAPIDIR}/bin/papi_command_line "PAPI_L2_TCM" >> ${DIR}/tcache_command_line.txt
-    ${PAPIDIR}/bin/papi_command_line "PAPI_L2_TCR" >> ${DIR}/tcache_command_line.txt
-    ${PAPIDIR}/bin/papi_command_line "PAPI_L3_TCA" >> ${DIR}/tcache_command_line.txt
-    ${PAPIDIR}/bin/papi_command_line "PAPI_L3_TCM" >> ${DIR}/tcache_command_line.txt
-
 done
